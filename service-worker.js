f4bd7e567f924a763e7c9ddce68f54380a022836
azorahai2689 <andywand01041958@gmail.com>
2026-05-21 13:56:01 +0200

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,29 +1,3 @@
-const CACHE_NAME = "geheime-missionen-v14";
-
-const FILES_TO_CACHE = [
-  "./",
-  "./index.html",
-  "./manifest.json",
-  "./icon.png"
-];
-
-self.addEventListener("install", event => {
-  self.skipWaiting();
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(FILES_TO_CACHE))
-  );
-});
-
-self.addEventListener("activate", event => {
-  event.waitUntil(
-    caches.keys()
-      .then(keys => Promise.all(keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))))
-      .then(() => self.clients.claim())
-  );
-});
-
-self.addEventListener("fetch", event => {
-  event.respondWith(
-    fetch(event.request).catch(() => caches.match(event.request))
-  );
+self.addEventListener('install', e => {
+  console.log('Service Worker installiert');
 });
